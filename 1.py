665f3d7197840d2c6aaa184ebe81e4c0a3a0313b
HAERIN07 <163218245+HAERIN07@users.noreply.github.com>
2024-03-20 11:07:21 +0900

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,26 +1,30 @@
-word="man"
+word="이태주"
 word=word.upper()
 print(word)
-word_show= "_"*len(word)
+word_show="-"*len(word)
 print(word_show)
 try_num=0
 ok_list=[]
 no_list=[]
 while True:
-    ans=input().upper()
+    ans = input().upper()
     print(ans)
-    result=word.find(ans)
-    print(result)
-    if result == -1:
+    resilt=word.find(ans)
+    print(resilt)
+    if resilt == -1 : 
         print("오답")
         try_num+=1
         no_list.append(ans)
-    else:
+    else :
         print("정답")
         ok_list.append(ans)
-        for i in range(ine(word)):
+        for i in range(len(word)):
             if word[i]==ans:
-                word_show=word_show[:i]+ans+word_show[i+1:]
+                word_show=word_show [:i] + ans + word_show[i+1:]
+                
         print(word_show)
-    if try_num==7: break
-    if word_show.find("_")==-1: break
+    if try_num == 7 : break
+    if word_show.find("_") == -1 : break
+
+
+
